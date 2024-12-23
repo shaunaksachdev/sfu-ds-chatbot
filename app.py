4bacb13759ec98eefc3af9ad88e2da803e81e024
Shaunak <112926391+shaunaksachdev@users.noreply.github.com>
2024-12-23 14:48:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, jsonify # type: ignore
+from chatbot import process_query
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    data = request.get_json()
+    user_query = data.get("query", "")
+    response = process_query(user_query)
+    return jsonify({"response": response})
+
+if __name__ == '__main__':
+    app.run(debug=True)
